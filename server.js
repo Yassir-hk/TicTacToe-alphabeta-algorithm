ce014ea24dc2468abd1a1faddc8563ae71521b48
Yassir-hk <theencryptionmind@gmail.com>
2024-04-22 18:03:46 +0100

    _

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 5000;
+const port = 3000;
 
+// Serve static files from the "root" directory
 app.use(express.static(path.join(__dirname, './')));
 
+// Route to serve the index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/', 'index.html'));
+  res.sendFile(path.join(__dirname, './', 'index.html'));
 });
 
+// Console message
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
