86d0f993154a5609cc483f59c19175f79c38bb57
Yassir Hakkou <124474286+Yassir-hk@users.noreply.github.com>
2024-05-14 20:37:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "root" directory
 app.use(express.static(path.join(__dirname, './')));
 
-// Route to serve the index.html file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './', 'index.html'));
 });
 
-// Console message
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
